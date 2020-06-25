7e16b1002fe063a10e431e11658abc5b12b55bcf
Binghui Liu <bertieliu52@gmail.com>
2020-06-25 16:24:03 +0300

    add a progressbar

--- src/index.js
+++ src/index.js
@@ -122,3 +122,19 @@ table.onclick = function(event) {
     }
   }
 };
+
+move();
+
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 1;
+  var id = setInterval(frame, 1000);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + "%";
+    }
+  }
+}
